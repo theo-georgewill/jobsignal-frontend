1b12b94a982358ea636c4587e0a413a11c212788
Theo <ttggwll@gmail.com>
2026-04-21 18:26:18 +0100

    update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,29 @@
 import js from "@eslint/js";
 import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   js.configs.recommended,
 
+  ...tseslint.configs.recommended,
+
   {
-    files: ["src/**/*.{ts,tsx,js,jsx}"],
+    files: ["src/**/*.{ts,tsx}"],
     languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
       globals: globals.browser,
     },
   },
 
   {
-    files: ["vite.config.ts"],
+    files: ["vite.config.ts", "tailwind.config.ts"],
     languageOptions: {
+      parser: tseslint.parser,
       globals: globals.node,
     },
   },
